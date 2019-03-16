27f234074ccebe22401cf81ce84ac4dc4923f7e3
rraman2 <rraman2@ncsu.edu>
2019-03-16 13:05:41 -0400

    sudo changes

--- milestone2/ansible_scripts/roles/iTrust_fuzzer/templates/fuzzer.js
+++ milestone2/ansible_scripts/roles/iTrust_fuzzer/templates/fuzzer.js
@@ -1,46 +1,98 @@
+ 
 const fs = require('fs')
-const path = require('path');
-var Random = require("random-js");
+const path = require('path')
 const child_process = require('child_process')
 var http = require('http');
 
-var files= [];
-var count=10;
-var getFiles= function (dir, done) {
-    let files = [];
+const walkSync = (dir, filelist = []) => {
+    fs.readdirSync(dir).forEach(file => {
+        filelist = fs.statSync(path.join(dir, file)).isDirectory()
+            ? walkSync(path.join(dir, file), filelist)
+            : filelist.concat(path.join(dir, file));
+    });
+    return filelist;
+}
 
-    fs.readdir(dir, function(err, list) {
-        if (err) return done(err);
+const getJavaFilePaths = (dirPath)=>{
+    let filePaths = walkSync(dirPath)
+    let javaPaths = []
 
-        var pending = list.length;
+    filePaths.forEach(file => {
+        if (!file.match(/models/) && !file.match(/sql/) && path.basename(file).match(/[a-zA-Z0-9._/]+[.]java$/g)) {
+            javaPaths.push(file)
+        }
+    })
+    return javaPaths;
+}
 
-        if (!pending) return done(null, files);
+const fileFuzzer = (filePath) => {
+    let lines = fs.readFileSync(filePath, 'utf8').split(/\r?\n/)
+    fs.writeFileSync(filePath, '', {encoding:'utf8'});
 
-        list.forEach(function(file){
-            file = path.resolve(dir, file);
-
-            fs.stat(file, function(err, stat){
-                // If directory, execute a recursive call
-                if (stat && stat.isDirectory()) {
-                    // Add directory to array [comment if you need to remove the directories from the array]
-                    if(file.split('.')[1]=="java"){
-                    files.push(file);
-                    }
-
-                    getFiles(file, function(err, res){
-
-
-                        files = files.concat(res);
-                        
-                        
-                        if (!--pending) done(null, files);
-                    });
-                } else {
-                    if(file.split('.')[1]=="java"){
-                    files.push(file);}
-
-                    if (!--pending) done(null, files);
+    var prob=0;
+        lines.forEach(function(line){
+            prob=Math.random();
+            if(prob>0.4)
+            {
+              if(line.match('while') || line.match('for') || line.match('if'))
+              {
+                
+                if(line.match('<')){
+                  line=line.replace('<', '>');
                 }
+                else if(line.match('>') && !line.match('->')){
+                  line=line.replace('>', '<');
+                }
+<<<<<<< HEAD
+    
+              }
+            }
+            prob=Math.random();
+            if(prob>0.4){
+              if(line.match('=='))
+                line=line.replace(/==/g,'!=');
+              else if(line.match('!='))
+                line=line.replace(/!=/g,'==');
+              
+            }
+            prob=Math.random();
+            if(prob>0.4)
+            {
+              if((line.match('while') || line.match('for') || line.match('if')) && line.match(/[0]/))
+              {
+                line=line.replace(/[0]/g,"1");
+              }
+              else if((line.match('while') || line.match('for') || line.match('if')) && line.match('1'))
+              { 
+                  line=line.replace(/[1]/g,'0');
+              }
+
+            }
+            prob=Math.random();
+            if(prob>0.4)
+            {
+              if(line.match('for'))
+              {
+                if(line.match(/\+\+/)){
+                  line=line.replace(/\+\+/g, "--");
+                }
+                else if(line.match("--")){
+                  line=line.replace("--", "++");
+                }
+              }
+            }
+
+            prob=Math.random();
+            if(prob>0.4)
+            {
+                if(line.match('true')){
+                  line=line.replace('true', 'false');
+                }
+            }
+            else{
+                if(line.match('false')){
+                  line=line.replace('false', 'true');
+=======
             });
 
             
@@ -84,16 +136,16 @@ var fuzzer =
                 if( random().bool(0.25) )
                 {
                     line = line.replace('0', '1')
+>>>>>>> 3b312feb48111079cb60f99276879e576f0eb325
                 }
+            }
 
-                if(line != '\r')
-                line += '\n'
+            if(line != '\r')
+              line += '\n'
 
-                fs.appendFileSync(val,line);   
+            fs.appendFileSync(filePath,line);
+    
         });
-    }
-
-}
 }
 
 const commitFuzzer = (master_sha1, n) => {
@@ -104,22 +156,15 @@ const commitFuzzer = (master_sha1, n) => {
     return lastSha1;
 }
 
-const reverToFirstCommit = (firstSha1, n) => {
-    
-    child_process.execSync(`git checkout ${firstSha1}`)
-}
-
-const triggerJenkinsBuild = (jenkinsIP, jenkinsToken, githubURL, sha1) => {
+const triggerJenkinsBuild = (JENKINS_URL, jenkinsToken, githubURL, sha1) => {
     try {
-    console.log("\n $$$$$$$$$$$ SHA1: ",sha1);
-        console.log("http://${jenkinsIP}:8090/git/notifyCommit?url=${githubURL}&branches=fuzzer&sha1=${sha1}")
-        child_process.execSync(`curl "http://${jenkinsIP}:5001/git/notifyCommit?url=${githubURL}&branches=fuzzer&sha1=${sha1}"`)
+        console.log("http://${JENKINS_URL}:5001/git/notifyCommit?url=${githubURL}&branches=fuzzer&sha1=${sha1}")
+        child_process.execSync(`curl "http://${JENKINS_URL}:5001/git/notifyCommit?url=${githubURL}&branches=fuzzer&sha1=${sha1}"`)
         console.log(`Succesfully trigger build for fuzzer:${sha1}`)
     } catch (error) {
         console.log(`Couldn't trigger build for fuzzer:${sha1}`)
     }
 }
-
 
 const runFuzzingProcess = (n) => {
     let master_sha1 = process.env.MASTER_SHA1;
@@ -128,17 +173,16 @@ const runFuzzingProcess = (n) => {
     let jenkinsToken = process.env.JENKINS_BUILD_TOKEN;
     let githubURL = process.env.GITHUB_URL;
     for (var i = 0; i < n; i++) {
-        let javaPaths = getFiles('iTrust2/src/main/java/edu/ncsu/csc/itrust2',function(err,data){
-        reverToFirstCommit(sha1)
+        let javaPaths = getJavaFilePaths('iTrust2/src/main/java/edu/ncsu/csc/itrust2');
+        child_process.execSync(`git checkout -f ${sha1}`);
         javaPaths.forEach(javaPath =>{
             let rnd = Math.random();
-            if(rnd > 0.35)
-                fuzzer(javaPath);
-            });
+            if(rnd > 0.25)
+                fileFuzzer(javaPath);
         })
         let lastSha1 = commitFuzzer(master_sha1, i);
         triggerJenkinsBuild(jenkinsIP, jenkinsToken, githubURL, lastSha1)
     }
 }
 
-runFuzzingProcess(1);
+runFuzzingProcess(2);
